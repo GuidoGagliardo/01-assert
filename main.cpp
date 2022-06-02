38f41ace33167cca223da765b904e9cb5cfa3c46
Guido Gagliardo <ggagliardo@frba.utn.edu.ar>
2022-06-02 18:34:06 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+
+#include <cassert>
+
+
+int main() {
+    //BOOLEANOS
+    //assert(true);
+    //assert(not false);
+    //assert(not true);
+    assert(true or false);
+
+    //CHARACTERS
+    assert('a' != 'b');
+    assert(' ' + ' ' == '@');
+    assert('P'-'A'== 45-30);
+    assert(' '== 16*2);
+
+    //enteros
+       assert(100 == 100);
+       assert(123 != 34);
+
+    //pto flotante  (Reales) (float) (double) (long double)
+        assert(0.2<1);
+}
